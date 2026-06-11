26b238e80bc9f3f86307ad1d7ce179ed7251e5ce
TailerDerdan <anton.infosfera@gmail.com>
2026-06-11 13:34:11 +0300

    для 4-х роботов идеально работает с препятствиями со сглаживанием пути и с остановкой

--- code/src/robot/constants.h
+++ code/src/robot/constants.h
@@ -27,36 +27,6 @@ constexpr double STEP_DESCENT = 3e-7;
 constexpr double TIME_FOR_PREDICT = 2.0;
 
 constexpr double LARGE_LAMBDA = 3000.0;
-constexpr double WEIGHT_COST_TRACKING = 14.0;
-constexpr double WEIGHT_COST_OBSTACLES = 300.0;
+constexpr double WEIGHT_COST_TRACKING = 22.0;
+constexpr double WEIGHT_COST_OBSTACLES = 1400.0;
 constexpr double WEIGHT_COST_REPULSIVE = 800.0;
-
-// consts for 5 robots
-// constexpr size_t COUNT_CELL_X = 200;
-// constexpr size_t COUNT_CELL_Y = 100;
-//
-// constexpr size_t MAP_WIDTH = SIZE_CELL * COUNT_CELL_X;
-// constexpr size_t MAP_HEIGHT = SIZE_CELL * COUNT_CELL_Y;
-//
-// constexpr double RADIUS_A = 35;
-// constexpr double RADIUS_B = 25;
-// constexpr double RADIUS_ROBOT = 8;
-//
-// constexpr size_t MU = 2;
-// constexpr double EPSILON = 50;
-// constexpr double DT_SMOOTH_PATH = 2.0;
-//
-// constexpr double V_SPEED = 2.7;
-//
-// constexpr double GAMMA_REPULSIVE = 0.1;
-//
-// constexpr size_t COUNT_STEPS_GRADIENT = 25;
-// constexpr double STEP_GRADIENT = 0.009;
-// constexpr double STEP_DESCENT = 3e-7;
-//
-// constexpr double TIME_FOR_PREDICT = 2.1;
-//
-// constexpr double LARGE_LAMBDA = 5000.0;
-// constexpr double WEIGHT_COST_TRACKING = 11.7;
-// constexpr double WEIGHT_COST_OBSTACLES = 900.0;
-// constexpr double WEIGHT_COST_REPULSIVE = 10000000.0;
